4f8107639189bc6b0d842dfc0cdb6f3837341843
Michael Baker <99276772+MikeTB-Microsoft@users.noreply.github.com>
2023-01-27 10:32:32 -0500

    Updating Repo

--- src/jbossappser-construction/petstore-ee7/src/main/java/org/agoncal/application/petstore/model/UserRole.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.application.petstore.model;
-
-public enum UserRole {
-   // ======================================
-   // = Attributes =
-   // ======================================
-
-   USER, ADMIN
-}
